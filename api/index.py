686a4139ac33068f5180ce3768265843e485bb32
51sec <johnyan.ca@gmail.com>
2022-12-17 15:45:16 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,12 +10,9 @@ class handler(BaseHTTPRequestHandler):
 		self.send_header('Content-type','text/plain')
 		self.end_headers()
 
-		
-		
-        ip_addr1 = request.remote_addr
-        ip_addr2 = request.environ['REMOTE_ADDR']
-        ip_addr3 = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
-	
-	
-        return "</BR>" +"</BR>" +"</BR>" +"<h1>Welcome to My PY website!</h1>"+"</BR>" + "<h1> Your IP address is:" + ip_addr1 +"</BR>" + "<h1> Your IP address is:" + ip_addr2 + "</BR>" + "<h1> Your IP address is:" + ip_addr3
-
+        if "name" in dic:
+            message = "Hello, " + dic["name"] + "!"
+        else:
+            message = "Hello, stranger!"
+        self.wfile.write(message.encode())
+        return
